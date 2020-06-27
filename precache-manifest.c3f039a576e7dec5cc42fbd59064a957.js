f454e4d2049fe163ee5742b132fa705efe630db8
IgorokNerovnyi1980 <i.nerovnyi@gmail.com>
2020-06-27 17:30:44 +0300

    Updates

---
+++ precache-manifest.c3f039a576e7dec5cc42fbd59064a957.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbda6684da3c80b9842d18ae725cc85f",
+    "url": "/testForWeAreTheFuture/index.html"
+  },
+  {
+    "revision": "382460ada78a693a489b",
+    "url": "/testForWeAreTheFuture/static/js/2.de9e84bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/testForWeAreTheFuture/static/js/2.de9e84bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e95fb75984a8168c18a",
+    "url": "/testForWeAreTheFuture/static/js/main.a586aac6.chunk.js"
+  },
+  {
+    "revision": "d9c000048126a465c26b",
+    "url": "/testForWeAreTheFuture/static/js/runtime-main.dcd02576.js"
+  }
+]);
